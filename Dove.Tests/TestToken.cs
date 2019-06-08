07ab92e1a3def3efdb7c23088387c0c9cdc3f3ac
きーぼう <kibou811@gmail.com>
2019-06-08 10:27:47 +0900

    add TestNextToken3 method to TestToken.cs

--- Dove.Tests/TestToken.cs
+++ Dove.Tests/TestToken.cs
@@ -102,7 +102,36 @@ let result = add(five, ten);";
         [Fact]
         public void TestNextToken3()
         {
-            
+            var input = "1 == 1; 1 != 0; ><*/-=";
+
+            var testTokens = new List<Token>();
+            // 1 == 1;
+            testTokens.Add(new Token(TokenType.INT, "1"));
+            testTokens.Add(new Token(TokenType.EQ, "=="));
+            testTokens.Add(new Token(TokenType.INT, "1"));
+            testTokens.Add(new Token(TokenType.SEMICOLON, ";"));
+            // 1 != 0;
+            testTokens.Add(new Token(TokenType.INT, "1"));
+            testTokens.Add(new Token(TokenType.NOT_EQ, "!="));
+            testTokens.Add(new Token(TokenType.INT, "0"));
+            testTokens.Add(new Token(TokenType.SEMICOLON, ";"));
+            // ><*/-=
+            testTokens.Add(new Token(TokenType.GT, ">"));
+            testTokens.Add(new Token(TokenType.LT, "<"));
+            testTokens.Add(new Token(TokenType.ASTERISK, "*"));
+            testTokens.Add(new Token(TokenType.SLASH, "/"));
+            testTokens.Add(new Token(TokenType.MINUS, "-"));
+            testTokens.Add(new Token(TokenType.ASSIGN, "="));
+            testTokens.Add(new Token(TokenType.EOF, '\0'.ToString()));
+
+            var lexer = new Lexer(input);
+
+            foreach (var testToken in testTokens)
+            {
+                var token = lexer.NextToken();
+                Assert.Equal(testToken.Type, token.Type);
+                Assert.Equal(testToken.Literal, token.Literal);
+            }
         }
     }
 }
