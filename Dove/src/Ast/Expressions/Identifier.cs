e31c6fea6013413f2c3642f18895f3ed825f8a6d
きーぼう <kibou811@gmail.com>
2019-06-13 20:35:58 +0900

    add class Dove.Ast.Statements.LetStatement and Dove.Ast.Expressions.Identifier

---
+++ Dove/src/Ast/Expressions/Identifier.cs
@@ -0,0 +1,19 @@
+using Dove.Ast;
+using Dove.Lexing;
+
+namespace Dove.Ast.Expressions
+{
+    public class Identifier : IExpression
+    {
+        public Token Token { get; set; }
+        public string Value { get; set; }
+
+        public Identifier(Token token, string value)
+        {
+            this.Token = token;
+            this.Value = value;
+        }
+
+        public string TokenLiteral() => this.Token.Literal;
+    }
+}
---
+++ Dove/src/Ast/Statements/LetStatement.cs
@@ -0,0 +1,15 @@
+using Dove.Ast;
+using Dove.Ast.Expressions;
+using Dove.Lexing;
+
+namespace Fove.Ast.Statements
+{
+    public class LetStatement : IStatement
+    {
+        public Token Token { get; set; }
+        public Identifier Name { get; set; }
+        public IExpression Value { get; set; }
+
+        public string TokenLiteral() => this.Token.Literal;
+    }
+}
