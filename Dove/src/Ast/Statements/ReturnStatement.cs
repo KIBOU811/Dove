f226cdc06f3906eda65f9acf0b7c9340cf5c9bae
KIBOU811 <kibou811@gmail.com>
2019-06-21 11:08:49 +0900

    add method 'ToCode' in ReturnStatement class

--- Dove/src/Ast/Statements/ReturnStatement.cs
+++ Dove/src/Ast/Statements/ReturnStatement.cs
@@ -8,5 +8,15 @@ namespace Dove.Ast.Statements
         public IExpression ReturnValue { get; set; }
 
         public string TokenLiteral() => this.Token.Literal;
+        
+        public string ToCode()
+        {
+            var builder = new StringBuilder();
+            builder.Append(this.Token?.Literal ?? "");
+            builder.Append(" ");
+            builder.Append(this.ReturnValue?.ToCode() ?? "");
+            builder.Append(";");
+            return builder.ToString();
+        }
     }
 }
